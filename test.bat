2006df3744aafaae8a5f0992af647de3b2a749b2
Administrator <857899299@qq.com>
2019-07-06 14:56:17 +0800

    init

--- test.bat
+++ test.bat
@@ -10,10 +10,10 @@ adb shell "su -c 'cp sdcard/test.so /data/local/'"
 adb shell "su -c 'chmod 777 /data/local/test.so'"
 adb shell "su -c './data/local/test.so'"
 
-adb shell "su -c 'cat /proc/13638/maps'"
+::adb shell "su -c 'cat /proc/13638/maps'"
 
 ::adb push %~dp0app\build\intermediates\transforms\dexBuilder\debug\0\cn\wsgwz\myapplication\Jni.dex /sdcard/Jni.dex
 ::adb shell "su -c 'cp sdcard/Jni.dex /data/data/cn.wsgwz.myapplication/'"
 
 echo success!!
-pause
+::pause
